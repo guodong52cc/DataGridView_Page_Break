230d68ff755a9c5398a01a7b5c25c70116d90c82
guodong <gd@gd520.cc>
2023-06-23 19:22:08 +0800

    添加项目文件。

---
+++ DataGridView分页/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace DataGridView分页
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            panel1 = new Panel();
+            label1 = new Label();
+            buttonReadSql = new Button();
+            listBox1 = new ListBox();
+            buttonRefresh = new Button();
+            buttonRead = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btnPrev = new Button();
+            btnNext = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            comboBox2 = new ComboBox();
+            label5 = new Label();
+            label4 = new Label();
+            comboBox1 = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel1.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(933, 465);
+            dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(dataGridView1);
+            panel1.Location = new Point(126, 39);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(933, 465);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft YaHei UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(312, 193);
+            label1.Name = "label1";
+            label1.Size = new Size(292, 45);
+            label1.TabIndex = 4;
+            label1.Text = "未查询到相关数据";
+            // 
+            // buttonReadSql
+            // 
+            buttonReadSql.Location = new Point(12, 21);
+            buttonReadSql.Name = "buttonReadSql";
+            buttonReadSql.Size = new Size(94, 29);
+            buttonReadSql.TabIndex = 2;
+            buttonReadSql.Text = "读取文件";
+            buttonReadSql.UseVisualStyleBackColor = true;
+            buttonReadSql.Click += buttonReadSql_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 20;
+            listBox1.Location = new Point(12, 56);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(94, 264);
+            listBox1.TabIndex = 3;
+            listBox1.DoubleClick += listBox1_DoubleClick;
+            // 
+            // buttonRefresh
+            // 
+            buttonRefresh.Location = new Point(12, 326);
+            buttonRefresh.Name = "buttonRefresh";
+            buttonRefresh.Size = new Size(94, 29);
+            buttonRefresh.TabIndex = 4;
+            buttonRefresh.Text = "刷新表";
+            buttonRefresh.UseVisualStyleBackColor = true;
+            buttonRefresh.Click += buttonRefresh_Click;
+            // 
+            // buttonRead
+            // 
+            buttonRead.Location = new Point(12, 361);
+            buttonRead.Name = "buttonRead";
+            buttonRead.Size = new Size(94, 29);
+            buttonRead.TabIndex = 5;
+            buttonRead.Text = "读取表";
+            buttonRead.UseVisualStyleBackColor = true;
+            buttonRead.Click += buttonRead_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(126, 16);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 20);
+            label2.TabIndex = 6;
+            label2.Text = "当前表：";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(201, 16);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 20);
+            label3.TabIndex = 6;
+            label3.Text = "未选择";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoSize = true;
+            flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(231, 0);
+            flowLayoutPanel1.Margin = new Padding(8, 0, 8, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Padding = new Padding(8);
+            flowLayoutPanel1.Size = new Size(16, 34);
+            flowLayoutPanel1.TabIndex = 7;
+            flowLayoutPanel1.WrapContents = false;
+            // 
+            // btnPrev
+            // 
+            btnPrev.AutoSize = true;
+            btnPrev.Location = new Point(128, 3);
+            btnPrev.Margin = new Padding(8, 3, 8, 0);
+            btnPrev.Name = "btnPrev";
+            btnPrev.Size = new Size(87, 30);
+            btnPrev.TabIndex = 8;
+            btnPrev.Text = "上一页";
+            btnPrev.UseVisualStyleBackColor = true;
+            btnPrev.Click += BtnPrev_Click;
+            // 
+            // btnNext
+            // 
+            btnNext.AutoSize = true;
+            btnNext.Location = new Point(263, 3);
+            btnNext.Margin = new Padding(8, 3, 8, 0);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(82, 30);
+            btnNext.TabIndex = 8;
+            btnNext.Text = "下一页";
+            btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += BtnNext_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.ColumnCount = 7;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle());
+            tableLayoutPanel1.Controls.Add(comboBox2, 6, 0);
+            tableLayoutPanel1.Controls.Add(label5, 5, 0);
+            tableLayoutPanel1.Controls.Add(btnNext, 4, 0);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 3, 0);
+            tableLayoutPanel1.Controls.Add(btnPrev, 2, 0);
+            tableLayoutPanel1.Controls.Add(label4, 0, 0);
+            tableLayoutPanel1.Controls.Add(comboBox1, 1, 0);
+            tableLayoutPanel1.Location = new Point(540, 514);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(519, 34);
+            tableLayoutPanel1.TabIndex = 9;
+            // 
+            // comboBox2
+            // 
+            comboBox2.Dock = DockStyle.Fill;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(416, 3);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(100, 28);
+            comboBox2.TabIndex = 10;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
+            label5.Location = new Point(356, 3);
+            label5.Margin = new Padding(3);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 28);
+            label5.TabIndex = 10;
+            label5.Text = "跳转：";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
+            label4.Location = new Point(3, 3);
+            label4.Margin = new Padding(3);
+            label4.Name = "label4";
+            label4.Size = new Size(54, 28);
+            label4.TabIndex = 9;
+            label4.Text = "每页：";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Dock = DockStyle.Fill;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.ImeMode = ImeMode.NoControl;
+            comboBox1.ItemHeight = 20;
+            comboBox1.Items.AddRange(new object[] { "5", "10", "20", "30", "50" });
+            comboBox1.Location = new Point(63, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(54, 28);
+            comboBox1.TabIndex = 10;
+            comboBox1.Text = "5";
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1071, 560);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(buttonRead);
+            Controls.Add(buttonRefresh);
+            Controls.Add(listBox1);
+            Controls.Add(buttonReadSql);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Panel panel1;
+        private Label label1;
+        private Button buttonReadSql;
+        private ListBox listBox1;
+        private Button buttonRefresh;
+        private Button buttonRead;
+        private Label label2;
+        private Label label3;
+        private Label labelCurrent;
+        private LinkLabel linkLabelDown;
+        private LinkLabel linkLabelUp;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnPrev;
+        private Button btnNext;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label4;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label5;
+    }
+}
---
+++ DataGridView分页/Form1.cs
@@ -0,0 +1,362 @@
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace DataGridView分页
+{
+    public partial class Form1 : Form
+    {
+        private string? connectionString; // 数据库连接字符串
+        private int totalRows; // 总行数
+        private int pageSize = 5; // 每页行数
+        private int totalPages; // 总页数
+        private int currentPage = 1; // 当前页数
+        private List<object[]>? chartData; // 数据集合
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (comboBox2.Items.Count == 0)
+            {
+                comboBox2.Items.Add("未选择表");
+                comboBox2.Text = "未选择表";
+            }
+        }
+
+        // 读取Sqlite文件
+        private void buttonReadSql_Click(object sender, EventArgs e)
+        {
+            // 弹出对话框，读取Microsoft.EntityFrameworkCore.Sqlite文件
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "db文件|*.db";
+            openFileDialog.InitialDirectory = "C:\\Users\\NUC\\OneDrive\\C#\\项目\\二维码质检\\QRChecker_winform\\QRChecker_winform\\bin\\Debug\\net6.0-windows";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Title = "请选择db文件";
+
+            // 将结果读取到dbPath中
+            string? dbPath = openFileDialog.ShowDialog() == DialogResult.OK ? openFileDialog.FileName : null;
+
+            // 如果没有选择文件，弹出提示
+            if (string.IsNullOrWhiteSpace(dbPath))
+            {
+                MessageBox.Show("请选择db文件");
+                return;
+            }
+
+            // 读取Microsoft.EntityFrameworkCore.Sqlite文件
+            connectionString = $"Data Source={dbPath}";
+
+            // 触发buttonRefresh_Click事件
+            buttonRefresh_Click(sender, e);
+        }
+
+        // 刷新表名
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            using (SqliteConnection conn = new SqliteConnection(connectionString))
+            {
+                // 打开连接
+                conn.Open();
+
+                // 判断是否打开成功
+                if (conn.State != ConnectionState.Open)
+                {
+                    // 打开失败，弹出提示
+                    MessageBox.Show("打开失败");
+                    return;
+                }
+
+                // 打开成功，执行查询
+                string sql = "SELECT name FROM sqlite_master WHERE type='table'";
+
+                using (SqliteCommand cmd = new SqliteCommand(sql, conn))
+                {
+                    // 执行查询
+                    using (SqliteDataReader reader = cmd.ExecuteReader())
+                    {
+                        // 清空listBox
+                        listBox1.Items.Clear();
+
+                        // 读取表名并添加到listBox中
+                        while (reader.Read())
+                        {
+                            string tableName = reader.GetString(0);
+                            listBox1.Items.Add(tableName);
+                        }
+                    }
+                }
+            }
+        }
+
+        // 读取数据
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("请选择表");
+                return;
+            }
+
+            using (SqliteConnection conn = new SqliteConnection(connectionString))
+            {
+                // 打开连接
+                conn.Open();
+
+                // 判断是否打开成功
+                if (conn.State != ConnectionState.Open)
+                {
+                    // 打开失败，弹出提示
+                    MessageBox.Show("打开失败");
+                    return;
+                }
+
+                // 显示表名
+                label1.Visible = false;
+                label3.Text = listBox1.SelectedItem.ToString();
+
+                // 获取表总行数
+                string sqlCount = $"SELECT COUNT(*) FROM {listBox1.SelectedItem}";
+                using (SqliteCommand cmd = new SqliteCommand(sqlCount, conn))
+                {
+                    totalRows = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+
+                // 计算总页数
+                totalPages = (int)Math.Ceiling((double)totalRows / pageSize);
+
+                // 如果跳转页数为空，将当前页重置为 1
+                if (comboBox2.SelectedItem == null)
+                {
+                    currentPage = 1; // 将当前页设置为 1
+                }
+
+                // 打开成功，执行查询
+                string sql = $"SELECT * FROM {listBox1.SelectedItem}";
+                using (SqliteCommand cmd = new SqliteCommand(sql, conn))
+                {
+                    // 执行查询
+                    using (SqliteDataReader reader = cmd.ExecuteReader())
+                    {
+                        // 清空dataGridView
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.Columns.Clear();
+
+                        // 创建列
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            dataGridView1.Columns.Add(reader.GetName(i), reader.GetName(i));
+                        }
+
+                        chartData = new List<object[]>();
+
+                        while (reader.Read())
+                        {
+                            object[] rowData = new object[reader.FieldCount];
+                            reader.GetValues(rowData);
+                            chartData.Add(rowData);
+                        }
+
+                        LoadDataForPage(currentPage);
+                        UpdatePageControls();
+                        UpdateJump();
+                    }
+                }
+            }
+        }
+
+        // 双击表名，触发buttonRead_Click事件
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            buttonRead_Click(sender, e);
+        }
+
+        private void UpdatePageControls()
+        {
+            flowLayoutPanel1.Controls.Clear(); // 清空之前的分页控件
+
+            btnPrev.Enabled = (currentPage > 1);
+            btnNext.Enabled = (currentPage < totalPages);
+
+            // 添加页码标签
+            int startPage = Math.Max(currentPage - 2, 1);
+            int endPage = Math.Min(currentPage + 2, totalPages);
+
+            // 添加第一页标签
+            if (startPage > 1)
+            {
+                Label lblFirst = new Label();
+                lblFirst.Text = "1";
+                lblFirst.Click += LblPage_Click;
+                lblFirst.AutoSize = true;
+
+                if (currentPage == 1)
+                {
+                    lblFirst.Font = new Font(lblFirst.Font, FontStyle.Bold | FontStyle.Underline);
+                }
+
+                flowLayoutPanel1.Controls.Add(lblFirst);
+            }
+
+            // 添加省略号
+            if (startPage > 2)
+            {
+                Label lblEllipsis1 = new Label();
+                lblEllipsis1.Text = "...";
+                lblEllipsis1.AutoSize = true;
+                flowLayoutPanel1.Controls.Add(lblEllipsis1);
+            }
+
+            // 添加页码
+            for (int i = startPage; i <= endPage; i++)
+            {
+                Label lblPage = new Label();
+                lblPage.Text = i.ToString();
+                lblPage.Click += LblPage_Click;
+                lblPage.AutoSize = true;
+
+                if (i == currentPage)
+                {
+                    lblPage.Font = new Font(lblPage.Font, FontStyle.Bold | FontStyle.Underline);
+                }
+
+                flowLayoutPanel1.Controls.Add(lblPage);
+            }
+
+            // 添加省略号
+            if (endPage < totalPages - 1)
+            {
+                Label lblEllipsis2 = new Label();
+                lblEllipsis2.Text = "...";
+                lblEllipsis2.AutoSize = true;
+                flowLayoutPanel1.Controls.Add(lblEllipsis2);
+            }
+
+            // 添加最后一页标签
+            if (endPage < totalPages)
+            {
+                Label lblLast = new Label();
+                lblLast.Text = totalPages.ToString();
+                lblLast.Click += LblPage_Click;
+                lblLast.AutoSize = true;
+
+                if (currentPage == totalPages)
+                {
+                    lblLast.Font = new Font(lblLast.Font, FontStyle.Bold | FontStyle.Underline);
+                }
+                flowLayoutPanel1.Controls.Add(lblLast);
+
+            }
+        }
+
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                LoadDataForPage(currentPage);
+                UpdatePageControls();
+            }
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            if (currentPage < totalPages)
+            {
+                currentPage++;
+                LoadDataForPage(currentPage);
+                UpdatePageControls();
+            }
+        }
+
+        private void LblPage_Click(object sender, EventArgs e)
+        {
+            Label lblPage = (Label)sender;
+            int page = int.Parse(lblPage.Text);
+
+            if (currentPage != page)
+            {
+                currentPage = page;
+                LoadDataForPage(currentPage);
+                UpdatePageControls();
+            }
+        }
+
+
+        private void LoadDataForPage(int page)
+        {
+            // 根据当前页加载数据
+            int startIndex = (page - 1) * pageSize;
+            int endIndex = Math.Min(startIndex + pageSize - 1, totalRows - 1);
+
+            dataGridView1.Rows.Clear(); // 清空 dataGridView1 中的数据
+
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                object[] rowData = chartData[i];
+                dataGridView1.Rows.Add(rowData);
+            }
+        }
+
+        private SqliteDataReader? ExecuteDataReader(string sql)
+        {
+            SqliteDataReader? reader = null;
+            try
+            {
+                using (SqliteConnection conn = new SqliteConnection(connectionString))
+                {
+                    conn.Open();
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        using (SqliteCommand cmd = new SqliteCommand(sql, conn))
+                        {
+                            reader = cmd.ExecuteReader();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("执行查询时出错: " + ex.Message);
+            }
+            return reader;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pageSize = int.Parse(comboBox1.SelectedItem.ToString());
+            if (listBox1.SelectedItem != null)
+            {
+                buttonRead_Click(sender, e);
+            }
+        }
+
+        private void UpdateJump()
+        {
+            comboBox2.Items.Clear();
+            comboBox2.Text = string.Empty;
+            if (listBox1.SelectedItem != null)
+            {
+                for (int i = 0; i < totalPages; i++)
+                {
+                    comboBox2.Items.Add(i + 1);
+                }
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                currentPage = int.Parse(comboBox2.SelectedItem.ToString());
+                if (listBox1.SelectedItem != null)
+                {
+                    buttonRead_Click(sender, e);
+                }
+            }
+        }
+    }
+}
